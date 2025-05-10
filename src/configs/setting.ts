279cf42503fa082e408a3edd9516c17f5c203d14
Sajjad Shadrooz <shadruz2013@gmail.com>
2025-05-10 19:34:14 +0330

    #20: use application name from setting.

--- src/configs/setting.ts
+++ src/configs/setting.ts
@@ -3,4 +3,5 @@ export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // Information
-export const applicationName = process.env.NEXT_PUBLIC_APP_NAME;
+export const applicationName =
+  process.env.NEXT_PUBLIC_APP_NAME || "ArvanCloud Challenge";
