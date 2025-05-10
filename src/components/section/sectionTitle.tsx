f6e8953d266d22ad62332e47dda9a8dafcbb19a0
Sajjad Shadrooz <shadruz2013@gmail.com>
2025-05-10 21:54:50 +0330

    #9: Section title style bug fixed.

--- src/components/section/sectionTitle.tsx
+++ src/components/section/sectionTitle.tsx
@@ -8,7 +8,7 @@ interface SectionTitleProps {
 export const SectionTitle = ({ title, description }: SectionTitleProps) => {
   return (
     <div className="flex flex-col gap-2 bg-transparent">
-      <h3>{title}</h3>
+      <div className="text-lg font-semibold">{title}</div>
       {description && (
         <p className="text-sm text-neutral-default-1">{description}</p>
       )}
