d7ce6d4278d8130e10fda197f475df53348ab930
Sajjad Shadrooz <shadruz2013@gmail.com>
2025-05-12 20:02:08 +0330

    #38: button component padding implemented  same as the design kit.

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -30,11 +30,11 @@ export const Button = ({
       className={clsx(
         "rounded-xl p-2.5 flex items-center justify-center text-sm font-semibold",
         {
-          "bg-primary-fg text-white hover:bg-primary-hover-bg-2 active:bg-primary-press-bg-2":
+          "bg-primary-fg text-white hover:bg-primary-hover-bg-2 active:bg-primary-press-bg-2 px-4":
             variant === "primary",
-          "bg-red-600 text-white hover:bg-red-700 focus:ring-red-500":
+          "bg-red-600 text-white hover:bg-red-700 focus:ring-red-500 px-4":
             variant === "danger",
-          "bg-transparent border border-neutral-default-2 hover:border-neutral-hover-2 active:border-neutral-press-2":
+          "bg-transparent border border-neutral-default-2 hover:border-neutral-hover-2 active:border-neutral-press-2 px-4":
             variant === "info",
 
           "w-10 h-10 p-2": isIconOnly,
